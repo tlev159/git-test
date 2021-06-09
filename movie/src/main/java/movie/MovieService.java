e64672365c4aa3f821641d882af67159eb8d6e89
LigetiKaroly <ligeti.karoly78@gmail.com>
2021-06-09 16:52:06 +0200

    Add MovieService

--- movie/src/main/java/movie/MovieService.java
+++ movie/src/main/java/movie/MovieService.java
@@ -7,5 +7,18 @@ public class MovieService {
 
     private List<Movie> movies = new ArrayList<>();
 
-    public void save()
+    public void save(Movie movie) {
+        movies.add(movie);
+    }
+
+    public Movie findNewest() {
+        return movies.stream()
+                .max(m-> m.getReleaseDate());
+    }
+
+    public List<Movie> findByNamePart(String namePart) {
+        return movies.stream()
+                .filter(m -> m.getName().contains(namePart))
+                .toList();
+    }
 }
